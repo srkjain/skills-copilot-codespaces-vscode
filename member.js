72a1b72c3c1dcf2fd11cf0892c11a555775cc4ab
srkjain <srkjain@gmail.com>
2025-02-22 18:01:53 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    return {
+        name: 'John Doe',
+        age: 30,
+        skills: ['HTML', 'CSS', 'JS'],
+        // Method
+        getSkills: function() {
+            return this.skills;
+        }
+    }
+}git 
